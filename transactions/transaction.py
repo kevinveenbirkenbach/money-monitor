792b29cadcc4e6737070c76c0e875770e1756c73
Kevin Veen-Birkenbach <kevin@veen.world>
2025-03-01 13:10:47 +0100

    Optimized logic for transactions

--- transactions/transaction.py
+++ transactions/transaction.py
@@ -13,16 +13,11 @@ class Transaction:
         self.id = self.generate_id()
 
     def generate_id(self):
-        # Erzeuge den SHA256-Digest
         digest = hashlib.sha256(f"{self.date}_{self.description}_{self.amount}_{self.account}_{self.file_path}_{self.bank}".encode()).digest()
-        # Base32-Codierung liefert nur Großbuchstaben und Ziffern. Entferne eventuelle Padding-Zeichen.
         hash_base32 = base64.b32encode(digest).decode('utf-8').rstrip('=')
-        # Kürze oder padde auf feste Länge (hier z. B. 18 Zeichen)
-        fixed_length = 18
-        if len(hash_base32) >= fixed_length:
-            return hash_base32[:fixed_length]
-        else:
-            return hash_base32.ljust(fixed_length, '0')
+        prefix="TI01"
+        fixed_length = 14
+        return prefix + hash_base32[:fixed_length]
 
     def to_list(self):
         return [self.date, self.description, self.amount, self.account, self.file_path, self.bank, self.id]
